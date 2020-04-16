5c64ee5cd33bf32138538fa801afee340681773e
Phyllis Kan <Phyllis Kan>
2020-04-16 17:37:46 -0500

    create pet object

---
+++ frontend/src/pet.js
@@ -0,0 +1,8 @@
+class Pet {
+    constructor(obj) {
+        this.pet_name = obj.pet_name
+        this.pet_age = obj.pet_age
+        this.health_concern = obj.health_concern
+        this.image = obj.image
+    }
+}
