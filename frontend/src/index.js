9ec53349516cc06d767f6fb64a5b6d97930a06d7
Phyllis Kan <Phyllis Kan>
2020-04-17 16:03:34 -0500

    change to bootstrap base style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,13 @@
 // test that we can get data from the backend
-const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
-    .then(response => response.json())
-    .then(parsedResponse => console.log(parsedResponse));
+document.addEventListener('DOMContentLoaded', function () {
+    // console.log("where js lives")
+    fetch("http://localhost:3000/pets")
+        .then(response => response.json())
+        .then()
+
+})
+
+// const BACKEND_URL = 'localhost:3000';
+// fetch(`${BACKEND_URL}/test`)
+//     .then(response => response.json())
+//     .then(parsedResponse => console.log(parsedResponse));
