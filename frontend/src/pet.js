90b7af9430532bed63bd0dcdc44e38b358c0c4fb
Phyllis Kan <Phyllis Kan>
2020-04-17 21:45:52 -0500

    comment out pet.js

--- frontend/src/pet.js
+++ frontend/src/pet.js
@@ -1,8 +1,14 @@
-class Pet {
-    constructor(obj) {
-        this.pet_name = obj.pet_name
-        this.pet_age = obj.pet_age
-        this.health_concern = obj.health_concern
-        this.image = obj.image
-    }
-}
+// document.addEventListener('DOMContentLoaded', function () {
+
+// })
+// class Pet {
+//     constructor(obj) {
+//         this.pet_name = obj.pet_name
+//         this.pet_age = obj.pet_age
+//         this.health_concern = obj.health_concern
+//         this.image = obj.image
+//     }
+
+
+
+// }
