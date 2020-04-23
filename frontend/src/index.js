cd84ff27bd5aea81bedcd5645ede439d13f90fd7
Phyllis Kan <Phyllis Kan>
2020-04-23 14:21:04 -0500

    add filter functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,63 +1,29 @@
 // test that we can get data from the backend
 document.addEventListener('DOMContentLoaded', function () {
     console.log("where js lives")
+    // make a function run at onload, loadPet function that will fetch all my /pets, will loop thru 
     loadAllPets();
     createPet();
+    eventForAllPetType();
 })
 
 // class Pet {
-//     constructor(pet_name, pet_age, health_concern, image_link) {
+//     constructor(pet_name, pet_age, health_concern, image_link, category) {
 //         this.pet_name = pet_name
 //         this.pet_age = pet_age
 //         this.health_concern = health_concern
 //         this.image_link = image_link
+//          this.category = category
 //     }
 
-// -- get pet data --
-async function loadAllPets() {
-    let request = await fetch("http://localhost:3000/pets")
-    let allPets = await request.json()
-    allPets.forEach(pet => createPetCard(pet))
-    //inputting pet as an argument to the createPetCard function
-}
-
-// -- get user data --
-async function getUserFromPetID(petID) {
-    let response = await fetch(`http://localhost:3000/pet-user/${petID}`)
-    let userData = await response.json()
-    return userData
-}
-
-// -- create Card --
-async function createPetCard(pet) {
-    // to create a card with html and insert pet data
-    // We do not know anything, until we can write it or say it out loud
-    // 1) keep talking
-    // 2) you have to ask me specific questions
-    // if (!pet.id) { debugger; }
-    const user = await getUserFromPetID(pet.id)
-    const petsCard = document.querySelector('#pet-container')
-    const newPet = document.querySelector('#pet-card-template').content.cloneNode(true)
-    newPet.querySelector(".img-thumbnail").src = pet.image_link || "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101032/112815935-stock-vector-no-image-available-icon-flat-vector-illustration.jpg?ver=6"
-    let petCardAttributes = newPet.querySelector(".flip-card-back").children
-    petCardAttributes[0].innerText = `Name: ${pet.pet_name}`
-    petCardAttributes[1].innerText = `Age: ${pet.pet_age}`
-    petCardAttributes[2].innerText = `Health Concern: ${pet.health_concern}`
-    petCardAttributes[3].innerText = `Owner: ${user.name}`
-    petCardAttributes[4].innerText = `Contact email: ${user.email}`
-    // debugger;
-    petsCard.appendChild(newPet)
-
-}
+//     static get allPets() {
+//         return pets
+//     }
+// }
 
 // -- form for new pet --
 function createPet() {
     const newPetForm = document.querySelector('#rehome-form')
-    // const newPetName = document.querySelector('#addPetName')
-    // const newPetAge = document.querySelector('#addPetAge')
-    // const newHealthInfo = document.querySelector('#addHealth')
-    // let newPet = new Pet(newPetName.value, newPetAge.value, newHealthInfo.value)
-
 
     newPetForm.addEventListener('submit', function (e) {
         console.log("form submitted")
@@ -86,31 +52,74 @@ function createPet() {
         })
             .then(response => response.json()) // .json() converts json into a plain javascript object
             .then(json => createPetCard(json)) //pass in my new function, passing the json as a arg(pets) i am calling. have access to all my pet data
+            .then(() => { newPetForm.reset() })
             .catch(e => console.log(e));
     })
 
 }
+// -- get pet data --
+async function loadAllPets(category) {
+    // if the pet card don't belong to that category, 
+    // then clear the pet cards and re insert the cards
+    if (!!document.querySelector(".pet-card")) {
+        let petCards = document.querySelectorAll(".pet-card")
+        for (card of petCards) {
+            card.remove()
+        }
+    }
+    // create and insert into html for every single one of the pet
+    let request = await fetch("http://localhost:3000/pets")
+    let allPets = await request.json()
 
-// function createPetCard(pet) => to create a card with html and insert pet data
+    if (category) {
+        allPets = allPets.filter(pet => pet.category === category)
+    }
+    // putting in all pet data into createPetCard, 
+    allPets.forEach(pet => createPetCard(pet));
+}
 
-// create custom route 
-// that custom route need to forward to your new action
-// this new action like show
+async function eventForAllPetType() {
+    const allBtn = document.getElementById("allBtn");
+    allBtn.addEventListener('click', () => loadAllPets())
+    const dogBtn = document.getElementById("dogsBtn");
+    dogBtn.addEventListener('click', () => loadAllPets("dog"))
+    const catBtn = document.getElementById("catsBtn");
+    catBtn.addEventListener('click', () => loadAllPets("cat"))
+    const ferretBtn = document.getElementById("ferretsBtn");
+    ferretBtn.addEventListener('click', () => loadAllPets("ferret"))
+    const horseBtn = document.getElementById("horsesBtn");
+    horseBtn.addEventListener('click', () => loadAllPets("horse"))
+    const otherCritterBtn = document.getElementById("otherCrittersBtn");
+    otherCritterBtn.addEventListener('click', () => loadAllPets("otherCritter"))
+}
 
-// js
-// create function that will create a card with json data, takes in 1 arg, (pets)
+// -- get user data --
 // fetch (new path/pet-user/${:id} of the pet) return the user
+async function getUserFromPetID(petID) {
+    let response = await fetch(`http://localhost:3000/pet-user/${petID}`)
+    let userData = await response.json()
+    return userData
+}
 
+// -- create Card --
+async function createPetCard(pet) {
+    // to create a card with html and insert pet data
 
-// after all this done, function take in data from my pet, 
-// make a function run at onload, loadPet function that will fetch all my /pets, will loop thru 
-// that array and putting in all pet data into createPetCard, create and insert into html for every single one of the pet
+    // if (!pet.id) { debugger; }
+    const user = await getUserFromPetID(pet.id)
+    const petsCard = document.querySelector('#pet-container')
+    const newPet = document.querySelector('#pet-card-template').content.cloneNode(true)
+    newPet.querySelector(".img-thumbnail").src = pet.image_link || "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101032/112815935-stock-vector-no-image-available-icon-flat-vector-illustration.jpg?ver=6"
+    let petCardAttributes = newPet.querySelector(".flip-card-back").children
+    petCardAttributes[0].innerText = `Name: ${pet.pet_name}`
+    petCardAttributes[1].innerText = `Age: ${pet.pet_age}`
+    petCardAttributes[2].innerText = `Health Concern: ${pet.health_concern}`
+    petCardAttributes[3].innerText = `Owner: ${user.name}`
+    petCardAttributes[4].innerText = `Contact email: ${user.email}`
+    // debugger;
+    petsCard.appendChild(newPet)
 
-
-
-
-
-
+}
 
 // class User {
 //     constructor(name, email) {
