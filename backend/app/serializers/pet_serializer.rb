ea00309638c4f56dc898ab84be9f38da710a8ee2
Phyllis Kan <Phyllis Kan>
2020-04-25 18:28:06 -0500

    remove serializer folder

--- backend/app/serializers/pet_serializer.rb
+++
@@ -1,6 +0,0 @@
-class PetSerializer
-    include FastJsonapi::ObjectSerializer
-    attributes :pet_name, :pet_age, :health_concern, :image_link
-    belongs_to :user
-
-end
--- backend/app/serializers/user_serializer.rb
+++
@@ -1,5 +0,0 @@
-class UserSerializer
-    include FastJsonapi::ObjectSerialzier
-    attributes :name, :email, :pets
-    has_many :pets
-end
