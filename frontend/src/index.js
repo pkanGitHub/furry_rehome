912f708487b761709b9426969a43101aaac55fab
Phyllis Kan <Phyllis Kan>
2020-04-25 21:00:17 -0500

    moving functions into OOJS class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,100 +1,130 @@
 // test that we can get data from the backend
 document.addEventListener('DOMContentLoaded', function () {
     // make a function run at onload, loadPet function that will fetch all my /pets, will loop thru 
-    loadAllPets();
-    createPet();
-    eventForAllPets();
+    Pet.loadAllPets();
+    Pet.createPet();
+    Pet.eventForAllPets();
 })
+
 
 class Pet {
     constructor(petObj) {
         this.id = petObj.id
-        this.userID = petObj.user_id
-        this.name = petObj.pet_name
-        this.age = petObj.pet_age
-        this.health = petObj.health_concern
-        this.img = petObj.image_link
+        this.user_id = petObj.user_id
+        this.pet_name = petObj.pet_name
+        this.pet_age = petObj.pet_age
+        this.health_concern = petObj.health_concern
+        this.image_link = petObj.image_link
         this.category = petObj.category
     }
-}
 
-// -- form for new pet --
-function createPet() {
-    const newPetForm = document.querySelector('#rehome-form')
+    static createPet() {
+        const newPetForm = document.querySelector('#rehome-form')
 
-    newPetForm.addEventListener('submit', function (e) {
-        console.log("form submitted")
-        e.preventDefault();
-        fetch('http://localhost:3000/pets', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(
-                {
-                    "user": {
-                        "name": e.target.querySelector('#addContactName').value,
-                        "email": e.target.querySelector('#addContactEmail').value,
-                        "pets_attributes": [{
-                            "category": e.target.elements.pet.value,
-                            "pet_name": e.target.elements.petName.value,
-                            "pet_age": e.target.elements.petAge.value,
-                            "health_concern": e.target.elements.petHealth.value,
-                            "image_link": e.target.elements.petImg.value
-                        }]
+        newPetForm.addEventListener('submit', function (e) {
+            console.log("form submitted")
+            e.preventDefault();
+            fetch('http://localhost:3000/pets', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        "user": {
+                            "name": e.target.querySelector('#addContactName').value,
+                            "email": e.target.querySelector('#addContactEmail').value,
+                            "pets_attributes": [{
+                                "category": e.target.elements.pet.value,
+                                "pet_name": e.target.elements.petName.value,
+                                "pet_age": e.target.elements.petAge.value,
+                                "health_concern": e.target.elements.petHealth.value,
+                                "image_link": e.target.elements.petImg.value
+                            }]
+                        }
                     }
-                }
-            )
+                )
+            })
+                .then(response => response.json()) // .json() converts json into a plain javascript object
+                .then(json => new Pet(json).createPetCard())
+                .then(() => { newPetForm.reset() })
+                .catch(e => console.log(e));
         })
-            .then(response => response.json()) // .json() converts json into a plain javascript object
-            .then(json => createPetCard(json)) //pass in my new function, passing the json as a arg(pets) i am calling. have access to all my pet data
-            .then(() => { newPetForm.reset() })
-            .catch(e => console.log(e));
-    })
 
-}
-// -- get pet data --
-async function loadAllPets(category) {
-    // if the pet card don't belong to that category, 
-    // then clear the pet cards and re insert the cards
-    if (!!document.querySelector(".pet-card")) {
-        let petCards = document.querySelectorAll(".pet-card")
-        for (card of petCards) {
-            card.remove()
+
+    }
+    // -- get pet data --
+    static async loadAllPets(category) {
+        // if the pet card don't belong to that category, 
+        // then clear the pet cards and re insert the cards
+        if (!!document.querySelector(".pet-card")) {
+            let petCards = document.querySelectorAll(".pet-card")
+            for (let card of petCards) {
+                card.remove()
+            }
         }
+        // create and insert into html for every single one of the pet
+        let request = await fetch("http://localhost:3000/pets")
+        let allPets = await request.json()
+
+        if (category) {
+            allPets = allPets.filter(pet => pet.category === category)
+        }
+        // putting in all pet data into createPetCard, 
+
+        allPets.forEach(petObj => {
+            let pet = new Pet(petObj)
+            // console.log(pet)
+            pet.createPetCard() // pet is the object instance of the pet class
+        });
     }
-    // create and insert into html for every single one of the pet
-    let request = await fetch("http://localhost:3000/pets")
-    let allPets = await request.json()
 
-    if (category) {
-        allPets = allPets.filter(pet => pet.category === category)
+    static async eventForAllPets() {
+        // pet buttons
+        const allBtn = document.getElementById("allBtn");
+        allBtn.addEventListener('click', () => Pet.loadAllPets())
+        const dogBtn = document.getElementById("dogsBtn");
+        dogBtn.addEventListener('click', () => Pet.loadAllPets("dog"))
+        const catBtn = document.getElementById("catsBtn");
+        catBtn.addEventListener('click', () => Pet.loadAllPets("cat"))
+        const ferretBtn = document.getElementById("ferretsBtn");
+        ferretBtn.addEventListener('click', () => Pet.loadAllPets("ferret"))
+        const horseBtn = document.getElementById("horsesBtn");
+        horseBtn.addEventListener('click', () => Pet.loadAllPets("horse"))
+        const otherCritterBtn = document.getElementById("otherCrittersBtn");
+        otherCritterBtn.addEventListener('click', () => Pet.loadAllPets("otherCritter"))
     }
-    // putting in all pet data into createPetCard, 
 
-    allPets.forEach(petObj => {
-        pet = new Pet(petObj)
-        // console.log(pet)
-        createPetCard(pet) // pet is the object instance of the pet class
-    });
+    // -- get user data --
+    // fetch (new path/pet-user/${:id} of the pet) return the user
+    async getUserFromPetID() {
+        let response = await fetch(`http://localhost:3000/pet-user/${this.id}`)
+        let userData = await response.json()
+        let user = new User(userData)
+        return user
+    }
 
-}
+    // -- create Card --
+    async createPetCard() {
+        // to create a card with html and insert pet data
+        // if (!pet.pet_name) { debugger; }
+        const user = await this.getUserFromPetID()
 
-async function eventForAllPets() {
-    // pet buttons
-    const allBtn = document.getElementById("allBtn");
-    allBtn.addEventListener('click', () => loadAllPets())
-    const dogBtn = document.getElementById("dogsBtn");
-    dogBtn.addEventListener('click', () => loadAllPets("dog"))
-    const catBtn = document.getElementById("catsBtn");
-    catBtn.addEventListener('click', () => loadAllPets("cat"))
-    const ferretBtn = document.getElementById("ferretsBtn");
-    ferretBtn.addEventListener('click', () => loadAllPets("ferret"))
-    const horseBtn = document.getElementById("horsesBtn");
-    horseBtn.addEventListener('click', () => loadAllPets("horse"))
-    const otherCritterBtn = document.getElementById("otherCrittersBtn");
-    otherCritterBtn.addEventListener('click', () => loadAllPets("otherCritter"))
+        const petsCard = document.querySelector('#pet-container')
+        const newPet = document.querySelector('#pet-card-template').content.cloneNode(true)
+        newPet.querySelector(".img-thumbnail").src = this.image_link || "img/NoImage.png"
+        let petCardAttributes = newPet.querySelector(".flip-card-back").children
+        petCardAttributes[0].innerText = `Name: ${this.pet_name}`
+        petCardAttributes[1].innerText = `Age: ${this.pet_age}`
+        petCardAttributes[2].innerText = `Health Concern: ${this.health_concern}`
+        petCardAttributes[3].innerText = `Owner: ${user.name}`
+        petCardAttributes[4].innerText = `Contact email: ${user.email}`
+        // debugger;
+        petsCard.appendChild(newPet)
+
+    }
+
 }
 
 class User {
@@ -103,31 +133,9 @@ class User {
         this.email = user.email
     }
 }
-// -- get user data --
-// fetch (new path/pet-user/${:id} of the pet) return the user
-async function getUserFromPetID(petID) {
-    let response = await fetch(`http://localhost:3000/pet-user/${petID}`)
-    let userData = await response.json()
-    let user = new User(userData)
-    return user
-}
+// static:
+// User.getUserFromPetID
 
-// -- create Card --
-async function createPetCard(pet) {
-    // to create a card with html and insert pet data
-
-    // if (!pet.id) { debugger; }
-    const user = await getUserFromPetID(pet.id)
-    const petsCard = document.querySelector('#pet-container')
-    const newPet = document.querySelector('#pet-card-template').content.cloneNode(true)
-    newPet.querySelector(".img-thumbnail").src = pet.img || "img/NoImage.png"
-    let petCardAttributes = newPet.querySelector(".flip-card-back").children
-    petCardAttributes[0].innerText = `Name: ${pet.name}`
-    petCardAttributes[1].innerText = `Age: ${pet.age}`
-    petCardAttributes[2].innerText = `Health Concern: ${pet.health}`
-    petCardAttributes[3].innerText = `Owner: ${user.name}`
-    petCardAttributes[4].innerText = `Contact email: ${user.email}`
-    // debugger;
-    petsCard.appendChild(newPet)
-
-}
+// regular method : use this if you want to use data from an instance of the class
+// user = new User()
+// user.getUserFromPetID
